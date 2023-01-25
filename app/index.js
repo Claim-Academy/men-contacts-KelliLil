5e48e02199f46628c28f57b5bc94f176dd33075b
Kelli Lilly <kelli.lilly05@gmail.com>
2023-01-25 15:27:46 -0600

    Import express to index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+import contactsRouter from "./contact/routes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/contacts", contactsRouter);
+
+app.listen(3000, () => {
+  console.info("Server is running on port 3000");
+});
